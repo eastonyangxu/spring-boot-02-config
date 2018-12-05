76220a30df793a8c28679703b330ae613a845a96
eastonyangxu <easton_00@163.com>
2018-12-05 23:54:13 +0800

    增加test.java

---
+++ src/main/java/com/easton/springboot/test.java
@@ -0,0 +1,12 @@
+package com.easton.springboot;
+
+/**
+ * Created by easton_yang on 2018/12/5
+ */
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("测试github");
+    }
+
+}
